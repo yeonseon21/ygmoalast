80c8f14b0037f2f67e37e099deb81fe91d413b07
yeonseonkim <loeyys1063@sookmyung.ac.kr>
2021-06-15 13:00:22 +0900

    first commit

--- ygmoaproject/settings.py
+++ ygmoaproject/settings.py
@@ -126,13 +126,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 AUTH_USER_MODEL ='main.User'
 
-STATICFILES_DIRS = [
-    # os.path.join(BASE_DIR, 'static'),
-    BASE_DIR / "static",
-]
+# STATICFILES_DIRS = [
+#     # os.path.join(BASE_DIR, 'static'),
+#     BASE_DIR / "static",
+# ]
 
 SESSION_EXPIRE_AT_BROWSER_CLOSE=True
 
